562aecb4474a156da915c96815f3c4256fb77aac
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-05-18 16:07:02 +0200

    organization of profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,15 @@
 import { Col, Container, Row } from "react-bootstrap";
 import ProfileHeader from "./ProfileHeader";
 import LinkedInProfileSidebar from "./SidebarProfile";
+import Footer from "../Footer/Footer";
+import NavbarL from "../Navbar/NavbarL";
+import Messages from "../Messages/Messages";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Profile() {
   return (
     <>
+      <NavbarL />
       <Container>
         <Row>
           <Col md={9}>
@@ -16,6 +20,8 @@ function Profile() {
           </Col>
         </Row>
       </Container>
+      <Messages />
+      <Footer />
     </>
   );
 }
