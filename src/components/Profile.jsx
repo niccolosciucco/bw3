cdb75800b8dfb476d926c1c3b642578c287fcef4
bilal lafdili <bilallafdili23@gmail.com>
2026-05-18 11:27:48 +0200

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Col, Container, Row } from "react-bootstrap"
 
 function Profile() {
-  return <></>
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col md={8}></Col>
+          <Col md={4}></Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default Profile
