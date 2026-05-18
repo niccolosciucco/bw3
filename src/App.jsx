05afa5f2ed0ea60315078bd010cc0f56059e2cf0
Noemi Coppotelli <liz.c2004@gmail.com>
2026-05-18 13:05:36 +0200

    feat: Landing components with LandinPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter, Routes, Route } from "react-router";
+import LandingPage from "./components/Landing/LandingPage"
+import LoginPage from "./components/Login/LoginPage"
+
+
 
 function App() {
-  return <></>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
