1a64373b4c62b8b8a2c4934369f5fff58b63ca4a
mohamed jaouad <jaouadmohamed080@gmail.com>
2026-05-18 19:32:24 +0100

    add profile to app

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Col, Container, Row } from "react-bootstrap"
+import ProfileHeader from "./ProfileHeader"
 
 function Profile() {
   return (
     <>
       <Container>
         <Row>
-          <Col md={8}></Col>
+          <Col md={8}>
+            <ProfileHeader />
+          </Col>
           <Col md={4}></Col>
         </Row>
       </Container>
